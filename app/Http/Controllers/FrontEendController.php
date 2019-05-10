afee7a2a90f1b73b60c884c57357e9f85790d40d
yahya <yahya.comr565@gmail.com>
2019-05-10 03:55:55 +0300

    setup

--- app/Http/Controllers/FrontEendController.php
+++ app/Http/Controllers/FrontEendController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Tag;
-use App\Category;
+use App\category;
 use App\Setting;
 use App\Post;
 
@@ -13,15 +13,15 @@ class FrontEendController extends Controller
     public function index(){
         return view('index')
         ->with('title', Setting::first()->site_name)
-        ->with('categories' , Category::take(5)->get() )
+        ->with('categories' , category::take(5)->get() )
         ->with('first_post' , Post::orderBy('created_at','desc')->first())
         ->with('second_post' , Post::orderBy('created_at', 'desc')->skip(1)->take(1)->get()->first())
         ->with('third_post' , Post::orderBy('created_at', 'desc')->skip(2)->take(1)->get()->first())
         ->with('fourth_post' , Post::orderBy('created_at', 'desc')->skip(3)->take(1)->get()->first())
         ->with('five_post' , Post::orderBy('created_at', 'desc')->skip(4)->take(1)->get()->first())
         ->with('sexes_post' , Post::orderBy('created_at', 'desc')->skip(5)->take(1)->get()->first())
-        ->with('career', Category::find(1))
-        ->with('tutoirals',Category::find(3))
+        ->with('career', category::find(1))
+        ->with('tutoirals',category::find(3))
         ->with('settings', Setting::first());
     }
     public function singlePost($slug)
@@ -36,7 +36,7 @@ class FrontEendController extends Controller
         return view('single')->with('post' , $post)
         ->with('title', $post->title)
         ->with('settings', Setting::first())
-        ->with('categories' , Category::take(5)->get() )
+        ->with('categories' , category::take(5)->get() )
         ->with('next', Post::find($next_id))
          ->with('prev' , Post::find($prev_id))
         ->with('tags', Tag::all());
@@ -44,12 +44,12 @@ class FrontEendController extends Controller
     
     public function category($id)
     {
-        $category = Category::find($id);
+        $category = category::find($id);
         
         return view('category')->with('category', $category)
                                ->with('title', $category->name)
                                ->with('settings', Setting::first())
-                                ->with('categories' , Category::take(5)->get() );
+                                ->with('categories' , category::take(5)->get() );
     }
     
     public function tag($id)
@@ -59,6 +59,6 @@ class FrontEendController extends Controller
         return view('tag')->with('tag', $tag)
                            ->with('title' , $tag->tag)
                            ->with('settings' ,Setting::first())
-                          ->with('categories' , Category::take(5)->get() );
+                          ->with('categories' , category::take(5)->get() );
     }
 }
